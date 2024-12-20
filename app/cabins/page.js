ffbf2bac0f7a4180f2c8d0a2fda8ca67c65215f7
Milan Pjevic <milanpjevic@icloud.com>
2024-12-21 00:35:03 +0100

    Adding Interactivity With Client Components

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,5 +1,7 @@
 /** @format */
 
+import Counter from "../components/Counter";
+
 export default async function Page() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
@@ -14,6 +16,8 @@ export default async function Page() {
           <li key={user.id}>{user.name}</li>
         ))}
       </ul>
+
+      <Counter users={data} />
     </div>
   );
 }
