53174ca04f0fb5a0f597cedc3c64f00fc59b55f5
Milan Pjevic <milanpjevic@icloud.com>
2024-12-21 17:51:50 +0100

    Loading and Optimising Fonts

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,15 @@ import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 import "@/app/_styles/globals.css";
 
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+console.log(josefin.className);
+
 export const metadata = {
   title: {
     template: "%s / The Wild Oasis",
@@ -17,7 +26,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
+      <body
+        className={` ${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+      >
         <header>
           <Logo />
         </header>
