fe9f28b4cc5775617515fe586ad3caff695bc69c
Milan Pjevic <milanpjevic@icloud.com>
2024-12-22 18:35:27 +0100

    Dynamic Route Segments: Building the Cabin Page

---
+++ app/_components/CabinList.js
@@ -0,0 +1,18 @@
+/** @format */
+import { getCabins } from "@/app/_lib/data-service";
+
+import CabinCard from "@/app/_components/CabinCard";
+
+export default async function CabinList() {
+  const cabins = await getCabins();
+
+  if (!cabins.length) return null;
+
+  return (
+    <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
+      {cabins.map((cabin) => (
+        <CabinCard cabin={cabin} key={cabin.id} />
+      ))}
+    </div>
+  );
+}
