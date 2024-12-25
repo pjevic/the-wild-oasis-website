6e5746cd71e3b603c2d98de2a13df79d68db4264
Milan Pjevic <milanpjevic@icloud.com>
2024-12-25 12:05:27 +0100

    Server Components in Client Components

--- app/_components/SelectCountry.js
+++ app/_components/SelectCountry.js
@@ -2,11 +2,10 @@
 
 import { getCountries } from "@/app/_lib/data-service";
 
-// Let's imagine your colleague already built this component 😃
-
 async function SelectCountry({ defaultCountry, name, id, className }) {
   const countries = await getCountries();
-  const flag = countries.find((country) => country.name === defaultCountry)?.flag ?? "";
+  const flag =
+    countries.find((country) => country.name === defaultCountry)?.flag ?? "";
 
   return (
     <select
