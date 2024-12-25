d40c5db9d8d79a819082ca11186cd9e6273a7dde
Milan Pjevic <milanpjevic@icloud.com>
2024-12-25 15:20:28 +0100

    Data Fetching Strategy for the Reservation Section

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -2,10 +2,13 @@
 
 import { notFound } from "next/navigation";
 import { supabase } from "./supabase";
+import { eachDayOfInterval } from "date-fns";
 
 export async function getCountries() {
   try {
-    const res = await fetch("https://restcountries.com/v2/all?fields=name,flag");
+    const res = await fetch(
+      "https://restcountries.com/v2/all?fields=name,flag"
+    );
     const countries = await res.json();
     return countries;
   } catch {
@@ -40,6 +43,49 @@ export async function getCabin(id) {
   if (error) {
     console.error(error);
     notFound();
+  }
+
+  return data;
+}
+
+export async function getBookedDatesByCabinId(cabinId) {
+  let today = new Date();
+  today.setUTCHours(0, 0, 0, 0);
+  today = today.toISOString();
+
+  // Getting all bookings
+  const { data, error } = await supabase
+    .from("bookings")
+    .select("*")
+    .eq("cabinID", cabinId)
+    .or(`startDate.gte.${today},status.eq.checked-in`);
+
+  console.log(data);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Bookings could not get loaded");
+  }
+
+  // Converting to actual dates to be displayed in the date picker
+  const bookedDates = data
+    .map((booking) => {
+      return eachDayOfInterval({
+        start: new Date(booking.startDate),
+        end: new Date(booking.endDate),
+      });
+    })
+    .flat();
+
+  return bookedDates;
+}
+
+export async function getSettings() {
+  const { data, error } = await supabase.from("settings").select("*").single();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Settings could not be loaded");
   }
 
   return data;
