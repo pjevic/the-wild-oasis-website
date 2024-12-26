55fc69e8df5f913b6290bc0a60873c68f31973d5
Milan Pjevic <milanpjevic@icloud.com>
2024-12-26 18:18:16 +0100

    Building a Custom Sing Out Button

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -2,8 +2,12 @@
 
 "use server";
 
-import { signIn } from "./auth";
+import { signIn, signOut } from "./auth";
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
+}
+
+export async function signOutAction() {
+  await signOut({ redirectTo: "/" });
 }
