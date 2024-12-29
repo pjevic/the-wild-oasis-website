50dc9884770341be4cdd21438c5ac1b89cc5410d
Milan Pjevic <milanpjevic@icloud.com>
2024-12-29 21:58:47 +0100

    Creating a New Reservation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -17,6 +17,34 @@ export async function signOutAction() {
   await signOut({ redirectTo: "/" });
 }
 
+export async function createBooking(bookingData, formData) {
+  console.log(formData);
+
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const newBooking = {
+    ...bookingData,
+    guestID: session.user.guestID,
+    numGuests: Number(formData.get("numGuests")),
+    observations: formData.get("observations").slice(0, 1000),
+    extrasPrice: 0,
+    totalPrice: bookingData.cabinPrice,
+    isPaid: false,
+    hasBreakfast: false,
+    status: "unconfirmed",
+  };
+
+  const { error } = await supabase.from("bookings").insert([newBooking]);
+
+  if (error) {
+    throw new Error("Booking could not be created");
+  }
+
+  revalidatePath(`/cabins/${bookingData.cabinID}}`);
+  redirect("/cabins/thankyou");
+}
+
 export async function updateGuest(formData) {
   const session = await auth();
   if (!session) throw new Error("You must be logged in");
